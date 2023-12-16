ed25794c92343678d5cdd105a5a6e802d7a4fae4
MrAsieru <28875845+MrAsieru@users.noreply.github.com>
2023-12-16 20:28:38 +0100

    chore: dividir variables entorno mongodb

--- mongodb/init-mongo.sh
+++ mongodb/init-mongo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mongosh <<EOF
-use  ${MONGODB_INITDB_DATABASE}
-db.createUser({user: "${MONGODB_SERVER_USER}", pwd: "${MONGODB_SERVER_USER_PASSWORD}", roles: [{role: 'readWrite', db: "${MONGODB_INITDB_DATABASE}"}, {role: 'dbAdmin', db: "${MONGODB_INITDB_DATABASE}"}]})
-db.createUser({user: "${MONGODB_API_USER}", pwd: "${MONGODB_API_USER_PASSWORD}", roles: [{role: 'read', db: "${MONGODB_INITDB_DATABASE}"}]})
+use gtfs
+db.createUser({user: "${MONGODB_SERVER_USER}", pwd: "${MONGODB_SERVER_USER_PASSWORD}", roles: [{role: 'readWrite', db: "gtfs"}, {role: 'dbAdmin', db: "gtfs"}]})
+db.createUser({user: "${MONGODB_API_USER}", pwd: "${MONGODB_API_USER_PASSWORD}", roles: [{role: 'read', db: "gtfs"}]})
 
 db.paradas.createIndex({"paradaPadre": 1})
 db.posiciones.createIndex({"fecha": 1, "idAgencia": 1})
