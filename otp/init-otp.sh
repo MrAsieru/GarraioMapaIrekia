81ef8101afdb75f95b31d5d11f41c3ffb61ba57c
MrAsieru <28875845+MrAsieru@users.noreply.github.com>
2023-12-21 19:21:59 +0100

    fix: docker compose e init-otp

--- otp/init-otp.sh
+++ otp/init-otp.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Si graph.obj es más nuevo que todos los archivos GTFS cargarlo directamente
-if [ $(find /var/garraiomapairekia/gtfs -name "*.zip" | wc -l) -gt 0 ] && [ $(find /var/garraiomapairekia/gtfs -name "*.zip" -newer /var/garraiomapairekia/graphs/graph.obj | wc -l) ]; then
+if [ $(find /var/garraiomapairekia/gtfs -name "*.zip" | wc -l) -gt 0 ] && { [ ! -f /var/garraiomapairekia/graphs/graph.obj ] || [ $(find /var/garraiomapairekia/gtfs -name "*.zip" -newer /var/garraiomapairekia/graphs/graph.obj | wc -l) -gt 0 ] ;} then
     cp -r /var/garraiomapairekia/osm/* /var/opentripplanner
     cp -r /var/garraiomapairekia/gtfs/* /var/opentripplanner
     /docker-entrypoint.sh --build --save --serve
