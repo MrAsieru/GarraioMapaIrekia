631311d58de8a73fa7b97071b6b1a5b4fcf0497b
MrAsieru <28875845+MrAsieru@users.noreply.github.com>
2023-12-26 11:04:52 +0100

    fix: docker y entrypoint otp

--- otp/init-otp.sh
+++ otp/init-otp.sh
@@ -2,6 +2,7 @@
 
 # Si graph.obj es más nuevo que todos los archivos GTFS cargarlo directamente
 if [ $(find /var/garraiomapairekia/gtfs -name "*.zip" | wc -l) -gt 0 ] && { [ ! -f /var/garraiomapairekia/graphs/graph.obj ] || [ $(find /var/garraiomapairekia/gtfs -name "*.zip" -newer /var/garraiomapairekia/graphs/graph.obj | wc -l) -gt 0 ] ;} then
+    rm -f /var/opentripplanner/*gtfs.zip
     cp -r /var/garraiomapairekia/osm/* /var/opentripplanner
     cp -r /var/garraiomapairekia/gtfs/* /var/opentripplanner
     /docker-entrypoint.sh --build --save --serve
